2aec7979490ddc36e4d21871b61ebcea50e831dd
logicweaver <jiyabansalbjb@gmail.com>
2025-07-30 20:38:08 +0530

    added describing comments

--- oops.java
+++ oops.java
@@ -1,18 +1,31 @@
+
 class Pen {
+    // propertiesof the class
     String color;
     String type;
 
+    // fuction to write -- something
     public void write() {
         System.out.println("writing something");
+    }
+
+    public void printinfo() {
+        // this - is used to call the methods- color and type
+        System.out.println(this.color);
+        System.out.println(this.type);
+    }
+
+    Pen(String color, String type) {
+        this.color = color;
+        this.type = type;
     }
 }
 
 public class oops {
     public static void main(String args[]) {
-        Pen pen1 = new Pen();
-        pen1.color = "blue";
-        pen1.type = "gel";
-
-        pen1.write();
+        Pen pen1 = new Pen("blue", "ball");
+        // pen1 calling the print info function tht contains their own functions to
+        // print
+        pen1.printinfo();
     }
 }
